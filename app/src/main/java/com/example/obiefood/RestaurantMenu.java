e1edd37198d74a565ae268ac436a446ef3f9f946
Andrew Li <ali@oberlin.edu>
2019-09-21 15:12:14 -0400

    Yeet

--- app/src/main/java/com/example/obiefood/RestaurantMenu.java
+++ app/src/main/java/com/example/obiefood/RestaurantMenu.java
@@ -6,9 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class RestaurantMenu extends AppCompatActivity {
 
+    double total = 0.0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.restaurant_menu);
+    }
+
+
+    public void bowlIncrement(){
+        total += 5;
+    }
+
+    public void bowlDecrement(){
+        total -= 5;
     }
 }
