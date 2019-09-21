3e57f3b5f618217cd9393d70f59d4c5dcda01ee7
Andrew Li <ali@oberlin.edu>
2019-09-21 17:33:19 -0400

    Kim's button working:

--- app/src/main/java/com/example/obiefood/MainActivity.java
+++ app/src/main/java/com/example/obiefood/MainActivity.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.app.Activity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button kimsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        kimsButton = findViewById(R.id.kimsButton);
+        kimsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        RestaurantMenu.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
