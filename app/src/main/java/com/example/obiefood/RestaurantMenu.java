cec1ddb7162c975a91088fcc4a53618db49dc518
Andrew Li <ali@oberlin.edu>
2019-09-21 12:23:10 -0400

    Create sub-activities

---
+++ app/src/main/java/com/example/obiefood/RestaurantMenu.java
@@ -0,0 +1,14 @@
+package com.example.obiefood;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RestaurantMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.restaurant_menu);
+    }
+}
