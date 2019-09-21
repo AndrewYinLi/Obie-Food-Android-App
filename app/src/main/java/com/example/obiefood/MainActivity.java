24c8f694e838cac7365951c2b37d643e20cb6e47
Andrew Li <ali@oberlin.edu>
2019-09-21 18:06:40 -0400

    Add bulgogi bowls

--- app/src/main/java/com/example/obiefood/MainActivity.java
+++ app/src/main/java/com/example/obiefood/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
-import android.app.Activity;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/obiefood/RestaurantMenu.java
+++ app/src/main/java/com/example/obiefood/RestaurantMenu.java
@@ -4,22 +4,65 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 public class RestaurantMenu extends AppCompatActivity {
 
-    double total = 0.0;
+    private double total = 0.0;
+    private Button bulgogiIncrement;
+    private Button bulgogiDecrement;
+    private Button order;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.restaurant_menu);
+
+        order = findViewById(R.id.order);
+
+        bulgogiIncrement = findViewById(R.id.bulgogi_plus);
+        bulgogiIncrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                TextView bulgogiCount = findViewById(R.id.bulgogiCount);
+                int bulgogiCountInt = Integer.parseInt(bulgogiCount.getText().toString());
+                bulgogiCount.setText(String.valueOf(bulgogiCountInt+1));
+                bowlIncrement();
+                setTotal();
+            }
+        });
+
+        bulgogiDecrement = findViewById(R.id.bulgogi_minus);
+        bulgogiDecrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                TextView bulgogiCount = findViewById(R.id.bulgogiCount);
+                int bulgogiCountInt = Integer.parseInt(bulgogiCount.getText().toString());
+                if(bulgogiCountInt > 0) {
+                    bulgogiCount.setText(String.valueOf(bulgogiCountInt - 1));
+                    bowlDecrement();
+                    setTotal();
+                }
+            }
+        });
     }
 
 
-    public void bowlIncrement(){
+    private void bowlIncrement(){
         total += 5;
     }
 
-    public void bowlDecrement(){
+    private void bowlDecrement(){
         total -= 5;
+    }
+
+    private void setTotal(){
+        String totalStr = String.valueOf(total);
+        totalStr += "0";
+        order.setText("Place Order | " + totalStr);
     }
 }
