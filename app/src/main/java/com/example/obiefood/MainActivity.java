97f28c9e9d5d75c11782b8117c7577ebebe16cb5
Andrew Li <ali@oberlin.edu>
2019-09-21 18:13:52 -0400

    Aesthetic is good

--- app/src/main/java/com/example/obiefood/MainActivity.java
+++ app/src/main/java/com/example/obiefood/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Switch `Activity` to `RestaurantMenu`
         kimsButton = findViewById(R.id.kimsButton);
         kimsButton.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/obiefood/RestaurantMenu.java
+++ app/src/main/java/com/example/obiefood/RestaurantMenu.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.content.Intent;
 
 public class RestaurantMenu extends AppCompatActivity {
 
@@ -15,13 +16,15 @@ public class RestaurantMenu extends AppCompatActivity {
     private Button bulgogiDecrement;
     private Button order;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.restaurant_menu);
 
-        order = findViewById(R.id.order);
 
+
+        // Add bulgogi bowls
         bulgogiIncrement = findViewById(R.id.bulgogi_plus);
         bulgogiIncrement.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +38,7 @@ public class RestaurantMenu extends AppCompatActivity {
             }
         });
 
+        // Subtract bulgogi bowls
         bulgogiDecrement = findViewById(R.id.bulgogi_minus);
         bulgogiDecrement.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,20 +53,35 @@ public class RestaurantMenu extends AppCompatActivity {
                 }
             }
         });
+
+        // Switch `Activity` to `ConfirmDelivery`
+        order = findViewById(R.id.order);
+        order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                // Start NewActivity.class
+                Intent myIntent = new Intent(RestaurantMenu.this,
+                        ConfirmDelivery.class);
+                startActivity(myIntent);
+            }
+        });
     }
 
-
+    // Encapsulation for adding bowls
     private void bowlIncrement(){
         total += 5;
     }
 
+    // Encapsulation for subtracting bowls
     private void bowlDecrement(){
         total -= 5;
     }
 
+    // Some hacky shit because doubles are stupid
     private void setTotal(){
         String totalStr = String.valueOf(total);
         totalStr += "0";
-        order.setText("Place Order | " + totalStr);
+        order.setText("Place Order | $" + totalStr);
     }
 }
